59261a79091d556f88dfab07e9c6b49ad3930f4a
Stuart Leal <stuart.leal23@gmail.com>
2025-05-27 00:23:17 -0600

    Add keystrokes to Modal

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -1,4 +1,5 @@
 interface Window {
     openDrawer: () => void;
     openModal: (imageUrl: string, filename: string, size: string, created: string) => void;
+    loadMoreImages: () => Promise<void>;
 } 
